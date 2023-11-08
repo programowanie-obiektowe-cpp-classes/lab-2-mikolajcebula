4c8780ae483552a38eaa64263e3f08fd0dec7dee
Mikołaj Cebula <99285995+mikolajcebula@users.noreply.github.com>
2023-11-08 20:54:49 +0100

    wersja_1.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,56 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    private:
+    Resource* resource;  // wskaźnik do zarządzanego zasobu
+
+public:
+    // konstruktor domyślny
+    ResourceManager() : resource(new Resource()) {}
+
+    // konstruktor kopiujący
+    ResourceManager(const ResourceManager& other) : resource(new Resource(*(other.resource))) {
+        ++copies;
+        ++live;
+    }
+
+    // operator przypisania
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            delete resource;  // zwalniamy obecny zasób
+            resource = new Resource(*(other.resource));  // alokujemy i kopiujemy nowy zasób
+            ++cp_asgn;
+        }
+        return *this;
+    }
+
+    // konstruktor przenoszący
+    ResourceManager(ResourceManager&& other) noexcept : resource(other.resource) {
+        other.resource = nullptr;  // zapobiegamy zwolnieniu tego samego zasobu przez obiekt źródłowy
+        ++moves;
+        ++live;
+    }
+
+    // operator przypisania przenoszący
+    ResourceManager& operator=(ResourceManager&& other) noexcept {
+        if (this != &other) {
+            delete resource;  // zwalniamy obecny zasób
+            resource = other.resource;  // przenosimy zasób
+            other.resource = nullptr;  // zapobiegamy zwolnieniu tego samego zasobu przez obiekt źródłowy
+            ++mv_asgn;
+        }
+        return *this;
+    }
+
+    // destruktor
+    ~ResourceManager() {
+        delete resource;  // zwalniamy zarządzany zasób
+        ++destr;
+        --live;
+    }
+
+    double get() const {
+        // metoda zwracająca wynik zasobu
+        return resource->get();
+    }
 };
